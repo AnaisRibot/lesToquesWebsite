17eb09ebcfe576efee84545169b88b1fd92f5796
AnaisRibot <anais.ducher@gmail.com>
2022-10-25 17:48:01 +0200

    fix props in header

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,26 @@
-import { Chef, FindUs, Gallery, Header, Contact, Laurels, SpecialMenu } from '../components/container';
-import { places, maps } from '../constants/data';
+import {
+  Chef,
+  FindUs,
+  Gallery,
+  Header,
+  Contact,
+  Laurels,
+  SpecialMenu,
+} from "../components/container";
+import { places, maps } from "../constants/data";
 
 export default function Home() {
   let now = new Date();
 
   return (
     <div>
-      <Header/>
+      <Header place={places[now.getDay()]} />
       <Chef />
       <SpecialMenu />
-      <FindUs
-        place={places[now.getDay()]}
-        map={maps[now.getDay()]}
-      />
+      <FindUs place={places[now.getDay()]} map={maps[now.getDay()]} />
       <Contact />
       <Gallery />
       <Laurels />
     </div>
-  )
+  );
 }
